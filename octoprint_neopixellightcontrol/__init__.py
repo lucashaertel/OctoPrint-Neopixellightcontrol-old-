342fa97ad1c05b706ed5a70981de6bba98087925
lucashaertel <60064690+lucashaertel@users.noreply.github.com>
2022-01-12 01:10:05 +0100

    more fixes

--- octoprint_neopixellightcontrol/__init__.py
+++ octoprint_neopixellightcontrol/__init__.py
@@ -168,7 +168,7 @@ class NeopixellightcontrolPlugin(octoprint.plugin.StartupPlugin,
 
     def hex_to_rgb(self, value):
         value = value.lstrip('#')
-        value = len(value)
+        lv = len(value)
         return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
     # ~~ Softwareupdate hook
