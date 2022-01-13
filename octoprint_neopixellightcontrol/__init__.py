0c7192d91d1c15db72a9d3de2863786b1eae4c83
unknown <lucas.haertel0211@gmail.com>
2022-01-13 22:33:25 +0100

    unimplement on settings save

--- octoprint_neopixellightcontrol/__init__.py
+++ octoprint_neopixellightcontrol/__init__.py
@@ -23,27 +23,30 @@ class NeopixellightcontrolPlugin(octoprint.plugin.SettingsPlugin,
         self.is_on = False
 
     def init_rgb(self):
-        self._logger.info("count " + self.ledCount)
-        self._logger.info("pin " + self.colorPin)
-        self._logger.info("freq " + self.freqHz)
-        self._logger.info("dma " + self.dmaChannel)
-        self._logger.info("invert " + self.invert)
-        self._logger.info("brightness " + self.brightness)
-        self._logger.info("ledChannel " + self.dmaChannel)
-        self.strip = Adafruit_NeoPixel(self.ledCount, self.colorPin, self.freqHz,
-                                       self.dmaChannel, self.invert, self.brightness, self.ledChannel)
+        self._logger.info("count " + self._settings.get_int(["led_count"]))
+        self._logger.info("pin " + self._settings.get_int(["color_pin"]))
+        self._logger.info("freq " + self._settings.get_int(["freqHz"]))
+        self._logger.info("dma " + self._settings.get_int(["led_dma"]))
+        self._logger.info("invert " + self._settings.get_boolean(["invert"]))
+        self._logger.info(
+            "brightness " + self._settings.get_int(["led_brightness"]))
+        self._logger.info(
+            "ledChannel " + self._settings.get_int(["led_channel"]))
+        self.strip = Adafruit_NeoPixel(self._settings.get_int(["led_count"]), self._settings.get_int(["color_pin"]), self._settings.get_int(["freqHz"]),
+                                       self._settings.get_int(["led_dma"]), self._settings.get_boolean(
+                                           ["invert"]), self._settings.get_int(["led_brightness"]), self._settings.get_int(["led_channel"]))
         self.strip.begin()
         self._logger.info("LEDs initialized")
 
     def update_rgb(self, colorHex, is_on):
         if(self.strip is not None):
-            rgb = self.hex_to_rgb(colorHex)
+            rgb=self.hex_to_rgb(colorHex)
             self._logger.info("RGB Value: " + rgb)
-            r = rgb[0]
+            r=rgb[0]
             self._logger.info("R Value: " + r)
-            g = rgb[1]
-            b = rgb[2]
-            col = Color(r, g, b)
+            g=rgb[1]
+            b=rgb[2]
+            col=Color(r, g, b)
             for i in range(self.strip.numPixels()):
                 self.strip.setPixelColor(i, col)
                 self.strip.show()
@@ -52,15 +55,15 @@ class NeopixellightcontrolPlugin(octoprint.plugin.SettingsPlugin,
 
     def on_settings_save(self, data):
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-        self._logger.info("On save triggered")
-        self.colorPin = self._settings.get_int(["color_pin"])
-        self.powerPin = self._settings.get_int(["power_ctrl"])
-        self.color = self._settings.get(["color"])
-        self.ledCount = self._settings.get_int(["led_count"])
-        self.dmaChannel = self._settings.get_int(["led_dma"])
-        self.brightness = self._settings.get_int(["led_brightness"])
-        self.ledChannel = self._settings.get_int(["led_channel"])
-        self.is_on = self._settings.get_boolean(["is_on"])
+        # self._logger.info("On save triggered")
+        # self.colorPin = self._settings.get_int(["color_pin"])
+        # self.powerPin = self._settings.get_int(["power_ctrl"])
+        # self.color = self._settings.get(["color"])
+        # self.ledCount = self._settings.get_int(["led_count"])
+        # self.dmaChannel = self._settings.get_int(["led_dma"])
+        # self.brightness = self._settings.get_int(["led_brightness"])
+        # self.ledChannel = self._settings.get_int(["led_channel"])
+        # self.is_on = self._settings.get_boolean(["is_on"])
         self._logger.info("triggering init")
         self.init_rgb()
 
@@ -103,18 +106,18 @@ class NeopixellightcontrolPlugin(octoprint.plugin.SettingsPlugin,
     def on_api_command(self, command, data):
         self._logger.info("CMD: " + command)
         if command == "update_color":
-            color = data.get('color', None)
+            color=data.get('color', None)
             if color != None:
-                self.color = color
+                self.color=color
         elif command == "turn_on":
-            self.is_on = True
+            self.is_on=True
         elif command == "turn_off":
-            self.is_on = False
+            self.is_on=False
         # self.update_rgb(self.color, self.is_on)
 
     def hex_to_rgb(self, value):
-        value = value.lstrip('#')
-        lv = len(value)
+        value=value.lstrip('#')
+        lv=len(value)
         return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
     # ~~ Softwareupdate hook
@@ -140,21 +143,21 @@ class NeopixellightcontrolPlugin(octoprint.plugin.SettingsPlugin,
         }
 
 
-__plugin_name__ = "Neopixellightcontrol Plugin"
+__plugin_name__="Neopixellightcontrol Plugin"
 
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
 # __plugin_pythoncompat__ = ">=2.7,<3" # only python 2
 # __plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
+__plugin_pythoncompat__=">=2.7,<4"  # python 2 and 3
 
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = NeopixellightcontrolPlugin()
+    __plugin_implementation__=NeopixellightcontrolPlugin()
 
     global __plugin_hooks__
-    __plugin_hooks__ = {
+    __plugin_hooks__={
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
     }
